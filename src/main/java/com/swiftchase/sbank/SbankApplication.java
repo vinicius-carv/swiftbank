2f89047ca5b7f8d33f5e85500aa7bf9fb930db1f
vinicius-carv <valves8956@gmail.com>
2023-03-28 20:47:09 -0300

    Initial commit

---
+++ src/main/java/com/swiftchase/sbank/SbankApplication.java
@@ -0,0 +1,13 @@
+package com.swiftchase.sbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbankApplication.class, args);
+	}
+
+}
